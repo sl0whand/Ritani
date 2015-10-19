038a2a92722bdb5c81424819e8ce4574db380b4c
Jeffrey Uslan <jduslan@gmail.com>
2015-10-19 12:45:59 -0700

    First Commit

---
+++ Ritani_connection.R
@@ -0,0 +1,26 @@
+library(DBI)
+library(RMySQL)
+library(dbConnect)
+
+con<-dbConnect(MySQL(),
+               user="attrread",
+               password="diamonds",
+               host="attribution.cxg3u5tpgfux.us-east-1.rds.amazonaws.com",
+               port=3306,
+               dbname="log_analysis"
+)
+
+tables=dbListTables(con)
+
+tablename=tables[1]
+query1=paste0("SHOW TABLES LIKE '",tablename,"'")
+dbGetQuery(con,query1)
+
+# dbWriteTable(con, "tablename", dataframe, overwrite = TRUE)
+query1=paste("SELECT COUNT(*) FROM",tablename)
+dbGetQuery(con,query1)
+
+data=dbReadTable(con, tablename)
+
+# dbRemoveTable(con, " tablename ")
+dbDisconnect(con)
