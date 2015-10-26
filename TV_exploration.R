70e1f5eabd1d116a8c1c7f249654059cdc67f79e
Jeffrey Uslan <jduslan@gmail.com>
2015-10-25 19:07:38 -0700

    10/25

--- TV_exploration.R
+++ TV_exploration.R
@@ -6,6 +6,8 @@ library(ggplot2)
 library(gridExtra)
 library(forecast)
 library(stats)
+library(pander)
+library(TTR)
 
 tv_data=tbl_df(read.xlsx("tvanalysis.xlsx",sheet=2,rows=c(1:95),cols=c(1,4:20)))
 tv_data$date=as.Date(as.character(tv_data$date),"%m/%d/%Y")+years(2000)
@@ -24,20 +26,7 @@ sub_study$paid.brand.sessions=as.numeric(as.character(sub_study$paid.brand.sessi
 
 
 #Plot relevant data
-tv_spend_plot=ggplot(sub_study) + theme_bw()+ 
-              geom_line(aes(x=date,y=tv.spend,color="TV Spending")) +
-              xlab("Date")+ylab("Dollars") +
-              guides(color=guide_legend(title=NULL))
-traffic_plot=ggplot(sub_study) + theme_bw()+
-              geom_line(aes(x=date,y=organic,color="Organic"))+ 
-              geom_line(aes(x=date,y=organic.home,color="Organic Home"))+ 
-              geom_line(aes(x=date,y=direct.home,color="Direct Home"))+ 
-              geom_line(aes(x=date,y=paid.brand.sessions,color="Paid Brand"))+
-              ggtitle("Traffic")+xlab("Date")+ylab("Visits")+
-              guides(color=guide_legend(title="Source"))
-
-grid.arrange(traffic_plot, tv_spend_plot, ncol = 1)
-
+ 
 #Correlations
 cor(sub_study[,sapply(sub_study,is.numeric)])[1,]
 vars<-c("organic","organic.home","direct.home","paid.brand.sessions")
@@ -60,82 +49,91 @@ CCF_plot<-ggplot(ccf.df,aes(x=Lag,y=Correlation,color=Source))+ theme_bw()+
 CCF_plot
 # ggplotly(CCF_plot)  
 
-#Add a lagged TV element at 7 weeks based on findings in CCF
-sub_study$tv_lag_7=c(rep(NA,7),sub_study$tv.spend[1:(length(sub_study$tv.spend)-7)])
-# organic+organic.home+direct.home+paid.brand.sessions
+## Organic Channel
+###2nd order polynomial fit
 
-##ACF explorations
-#organic
-stl_obj=stl(ts(sub_study$organic, frequency = 2),t.window=15,s.window="periodic",robust=TRUE)
+poly_fit=lm(data=sub_study,organic~tv.spend+I(tv.spend^2))
+tmp_df=data.frame(round(coef(poly_fit),3))
+tmp_df=rbind(tmp_df,cor(fitted(poly_fit),sub_study$organic)^2)
+rownames(tmp_df)[length(rownames(tmp_df))]="R2"
+pander(tmp_df)
+
+
+
+###Stepwise polynomial fit
+big_poly_fit=lm(data=sub_study,organic~tv.spend+I(tv.spend^2)+I(tv.spend^3)+I(tv.spend^4))
+stepwise_model=step(big_poly_fit,k=log(nrow(mtcars)))
+
+
+
+tmp_df=data.frame(round(coef(stepwise_model),3))
+tmp_df=rbind(tmp_df,cor(fitted(stepwise_model),sub_study$organic)^2)
+rownames(tmp_df)[length(rownames(tmp_df))]="R2"
+pander(tmp_df)
+
+
+tmp_df=data.frame(tv.spend=sub_study$tv.spend,channel=sub_study$organic,poly=fitted(poly_fit),
+                  step=fitted(stepwise_model))
+
+ggplot(tmp_df) + theme_bw()+
+  geom_point(aes(x=tv.spend,y=channel,color="Organic"))+ 
+  geom_line(aes(x=tv.spend,y=poly,color="2nd Order Plynomial Fit"))+ 
+  geom_line(aes(x=tv.spend,y=step,color="Stepwise Polynomial Fit"))+ 
+  ggtitle("Model Fits")+xlab("TV Speding (Dollars)")+ylab("Visits")+
+  guides(color=guide_legend(title="Models"))
+
+#This plot shows a major time trend component- implying previous models were useless
+plot(stepwise_model$residuals)
+
+#These plots shows a major auto-correlative component- we can include that in the model
+acf(stepwise_model$residuals,ylab="Correlation",main="Autocorrelation of Residuals")
+plot(sub_study$organic)
+plot(sub_study$organic,lag(sub_study$organic))
+
+#These plots shows a major difference component- we can include in the model
+pacf(stepwise_model$residuals,ylab="Correlation",main="Partial Autocorrelation of Residuals")
+plot(diff(stepwise_model$residuals))
+
+
+plot(sub_study$tv.spend,sub_study$organic)
+
+
+#
+plot(SMA(diff(stepwise_model$residuals),n=2))
+
+
+
+
+
+
+stl_obj=stl(ts(sub_study$organic, frequency = 26), s.window="periodic",robust=TRUE)
 plot(stl_obj)+title("Organic Seasonal Decomposition")
 
-lm_fit_organic=lm(organic~date,data=sub_study)
-acf(sub_study$organic)
-acf(lm_fit_organic$residuals)
-acf(diff(sub_study$organic))
+
+###Arima model with TV spend
+
+xreg_matrix<-model.matrix(formula(stepwise_model),data=sub_study)
+xreg_matrix=xreg_matrix[,-1]
+
+# tv_arima=Arima(sub_study$organic,order=c(1,1,0),xreg=xreg_matrix)
+
+tv_arima=auto.arima(sub_study$organic,xreg=xreg_matrix,max.p=10)
+# summary(tv_arima)
+tv_arima_fcast=forecast(tv_arima,xreg=xreg_matrix)
+plot(tv_arima_fcast)
 
 
-organic_arima=auto.arima(sub_study$organic)
-summary(organic_arima)
-organic_arima_fcast=forecast(organic_arima)
-plot(organic_arima_fcast)
-organic_arima_R2=round(cor(fitted(organic_arima_fcast),sub_study$organic)^2,2)
-organic_arima_R2
-
-organic_tv_arima=auto.arima(sub_study$organic,xreg=c(sub_study$tv.spend))
-summary(organic_tv_arima)
-organic_tv_arima_fcast=forecast(organic_tv_arima,xreg=c(sub_study$tv.spend))
-plot(organic_tv_arima_fcast)
-organic_tv_arima_R2=round(cor(fitted(organic_tv_arima_fcast),sub_study$organic)^2,2)
-organic_tv_arima_R2
+tv_arima_R2=round(cor(fitted(tv_arima_fcast),sub_study$organic)^2,2)
 
 
+tmp_df=data.frame(coef(tv_arima))
+tmp_df=rbind(tmp_df,tv_arima_R2)
+rownames(tmp_df)[length(rownames(tmp_df))]="R2"
+pander(tmp_df)
 
 
-#organic home
-
-
-lm_fit_organic.home=lm(organic.home~date,data=sub_study)
-acf(sub_study$organic.home)
-acf(lm_fit_organic.home$residuals)
-acf(diff(sub_study$organic.home))
-
-
-
-
-
-#direct home
-lm_fit_direct.home=lm(direct.home~date,data=sub_study)
-acf(sub_study$direct.home)
-acf(lm_fit_direct.home$residuals)
-acf(diff(sub_study$direct.home))
-
-
-# paid brand session
-lm_fit_paid.brand.sessions=lm(paid.brand.sessions~date,data=sub_study)
-acf(sub_study$paid.brand.sessions)
-acf(lm_fit_paid.brand.sessions$residuals)
-acf(diff(sub_study$paid.brand.sessions))
-
-
-##Simple Regressions
-
-study_vars=c("date","tv.spend","organic","organic.home","direct.home","paid.brand.sessions")
-all_fit=lm(data=sub_study,tv.spend~0+organic+organic.home+direct.home+paid.brand.sessions)
-coef(all_fit)
-cor(sub_study[,sapply(sub_study,is.numeric)])[5,]
-
-
-
-
-
-
-
-
-
-
-
-
-
+plot(fitted(poly_fit),sub_study$organic)
+plot(fitted(stepwise_model),sub_study$organic)
+plot(as.vector(fitted(tv_arima_fcast)),sub_study$organic)
 
 
